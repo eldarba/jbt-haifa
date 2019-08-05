0ea45f3bdb53f180eeed01d9f43cd3de4aad62ea
jbt <jbt@NewAIO>
2019-08-05 18:51:55 +0300

    hibernate

---
+++ eldar-hibernate-demo1/src/com/eldar/jdbc/TestJDBC.java
@@ -0,0 +1,13 @@
+package com.eldar.jdbc;
+
+public class TestJDBC {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost:3306/db1?useSSL=false@serverTimezone=UTC";
+		String user = "eldar1";
+		String password = "pass1";
+
+	}
+
+}
