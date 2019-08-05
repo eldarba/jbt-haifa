9aa111e076817adb7ea62f7ee47b1287412a7cf6
jbt <jbt@NewAIO>
2019-08-05 19:33:24 +0300

    hibernate

--- eldar-hibernate-demo1/src/com/eldar/jdbc/TestJDBC.java
+++ eldar-hibernate-demo1/src/com/eldar/jdbc/TestJDBC.java
@@ -1,12 +1,23 @@
 package com.eldar.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestJDBC {
 
 	public static void main(String[] args) {
 
-		String url = "jdbc:mysql://localhost:3306/db1?useSSL=false@serverTimezone=UTC";
+		String url = "jdbc:mysql://localhost:3306/db1?useSSL=false&serverTimezone=UTC";
 		String user = "eldar1";
 		String password = "pass1";
+
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
+			System.out.println("connected to: " + url);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("disconnected from: " + url);
 
 	}
 
