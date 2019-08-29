073d3586812b4755cde79e70b873aee2291f23e7
jbt <jbt@NewAIO>
2019-08-29 20:34:44 +0300

    aaa

--- com.spring.eldar.jpa/src/main/java/core/config/AppConfig.java
+++ com.spring.eldar.jpa/src/main/java/core/config/AppConfig.java
@@ -24,6 +24,7 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 @EnableTransactionManagement
 public class AppConfig {
 
+	// spring helper class for the config data
 	@Autowired
 	private Environment env;
 
--- com.spring.eldar.jpa/src/main/java/core/config/StudentDaoImplJPA.java
+++ com.spring.eldar.jpa/src/main/java/core/config/StudentDaoImplJPA.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 import org.springframework.context.annotation.Primary;
@@ -11,13 +12,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @Primary
+@Transactional
 public class StudentDaoImplJPA implements StudentDao {
 
 	@PersistenceContext // DI
 	private EntityManager em;
 
 	@Override
-	@Transactional
 	public Student getStudent(int id) {
 		Student student = em.find(Student.class, id);
 		return student;
@@ -25,20 +26,35 @@ public class StudentDaoImplJPA implements StudentDao {
 
 	@Override
 	public List<Student> getAllStudents() {
-		// TODO Auto-generated method stub
-		return null;
+		String hql = "from Student";
+		// type safe query
+		TypedQuery<Student> query = em.createQuery(hql, Student.class);
+		List<Student> students = query.getResultList();
+		return students;
 	}
 
 	@Override
-	@Transactional
 	public void saveStudent(Student student) {
 		em.persist(student);
 	}
 
+	/*
+	 * if you want to start and end a transaction:
+	 */
+	public void saveStudentNonTransactiona(Student student) {
+		try {
+			em.getTransaction().begin();
+			em.persist(student);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+	}
+
 	@Override
 	public void deleteStudent(int id) {
-		// TODO Auto-generated method stub
-
+		Student std = getStudent(id);
+		em.remove(std);
 	}
 
 	@Override
